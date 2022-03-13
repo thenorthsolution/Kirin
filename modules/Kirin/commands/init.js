4c4cc5adff15f13dcff0d2a9306cd750e663fbf9
GhexterCortes <ghextershumies@gmail.com>
2022-03-13 19:12:51 +0800

    update commands format

--- modules/Kirin/commands/init.js
+++ modules/Kirin/commands/init.js
@@ -10,27 +10,29 @@ const Kirin = require('../');
  */
 module.exports = (kirin) => {
     return {
-        command: new MessageCommandBuilder()
-        .setName('kirin-init')
-        .setDescription('Creates message with information about guildId, channelId, messageId')
-        .setExecute(async (args, message) => {
-            const guildId = message.guild.id;
-            const channelId = message.channel.id;
-            
-            const embed = new MessageEmbed()
-                .setTitle('Kirin init')
-                .setDescription('This message contains information about guildId, channelId, messageId')
-                .addField('guildId', guildId)
-                .addField('channelId', channelId)
-                .addField('messageId', `Please wait...`)
+        commands: [
+            new MessageCommandBuilder()
+            .setName('kirin-init')
+            .setDescription('Creates message with information about guildId, channelId, messageId')
+            .setExecute(async (args, message) => {
+                const guildId = message.guild.id;
+                const channelId = message.channel.id;
+                
+                const embed = new MessageEmbed()
+                    .setTitle('Kirin init')
+                    .setDescription('This message contains information about guildId, channelId, messageId')
+                    .addField('guildId', guildId)
+                    .addField('channelId', channelId)
+                    .addField('messageId', `Please wait...`)
 
-            const reply = await SafeMessage.send(message.channel, { content: ' ', embeds: [ embed ] });
-            
-            const messageId = reply.id;
-            embed.fields[2].value = messageId;
+                const reply = await SafeMessage.send(message.channel, { content: ' ', embeds: [ embed ] });
+                
+                const messageId = reply.id;
+                embed.fields[2].value = messageId;
 
-            await SafeMessage.edit(reply, { content: ' ', embeds: [ embed ] });
-            await SafeMessage.delete(message);
-        })
+                await SafeMessage.edit(reply, { content: ' ', embeds: [ embed ] });
+                await SafeMessage.delete(message);
+            })
+        ]
     };
 }
